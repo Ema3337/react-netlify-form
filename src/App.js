1465ff275841a73076869520e7129fd4d24b6115
unknown <goleemma0@gmail.com>
2022-08-22 15:39:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ContactForm from "./components/ContactForm" 
+function App() {
+  return (
+    <div className="App"> <ContactForm> </ContactForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactForm.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./ContactForm.css"
+
+function ContactForm() {
+    return <h1>form</h1>
+    
+}
+export default ContactForm
