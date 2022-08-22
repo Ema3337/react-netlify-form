12f69ba24b95279457e3894b53263caec8881c37
unknown <goleemma0@gmail.com>
2022-08-22 19:31:23 +0100

    contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,19 @@ import React from "react"
 import "./ContactForm.css"
 
 function ContactForm() {
-    return <h1>form</h1>
+    return ( <div className="contact">
+        <form name="contact" method="post" className="contact_form" data-netlify="true" onSubmit="submit" >
+        <input type="hidden" name="form-name" value="contact" />
+         <label for="name" >Votre nom complet: </label>  <input type="text" name="name" placeholder="Nom et Prenom"/>
+        <label for="mail">Votre email: </label><input type="email" name="email"placeholder="ex:vous@exemple.com"/>
+        <label for="genre">Choisissez votre recharge: </label> <select name="type">
+        <option value="tc">TRANSCARSH </option><option value="pcsa">PCSA </option><option value="neo">NEOSURFA </option>
+        </select>
+        
+            <button type="submit" className="mybut">Confirmer</button>
+         
+        </form>
+    </div> )
     
 }
 export default ContactForm
