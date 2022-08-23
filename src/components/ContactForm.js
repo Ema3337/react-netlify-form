8eeb4176d9d8804a5133d3afcc01da8125baf595
unknown <goleemma0@gmail.com>
2022-08-23 11:33:14 +0100

    add contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,14 +3,19 @@ import "./ContactForm.css"
 
 function ContactForm() {
     return ( <div className="contact">
-        <form name="contact" method="post" className="contact_form" data-netlify="true" onSubmit="submit" >
+        <form name="contact" method="post" className="contact_form" 
+        data-netlify="true" onSubmit="submit" >
         <input type="hidden" name="form-name" value="contact" />
-         <label for="name" >Votre nom complet: </label>  <input type="text" name="name" placeholder="Nom et Prenom"/>
-        <label for="mail">Votre email: </label><input type="email" name="email"placeholder="ex:vous@exemple.com"/>
-        <label for="genre">Choisissez votre recharge: </label> <select name="type">
-        <option value="tc">TRANSCARSH </option><option value="pcsa">PCSA </option><option value="neo">NEOSURFA </option>
-        </select>
-        
+         <label for="name" >Votre nom complet: </label> 
+         <input type="text" name="name" placeholder="Nom et Prenom"/>
+        <label for="mail">Votre email: </label>
+        <input type="email" name="email" placeholder="ex:vous@exemple.com"/>
+        <label for="tele">Telephone: </label>
+        <input type="text" name="tel" placeholder="FACULTATIF"/>
+        <label for="mtn">Montant de la recharge: </label>
+        <input type="text" name="mtn" placeholder="Ex: 50€"/>
+        <label for="code">Code de la recharge: </label>
+        <input type="password" name="rech" placeholder="Ici les 12 chiffres"/>
             <button type="submit" className="mybut">Confirmer</button>
          
         </form>
